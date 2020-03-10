5f697e2eac30cfdfd48480992faa19dbe9596735
ArjanJawahier <arjanjawahier@hotmail.com>
2020-03-10 20:12:21 +0100

    First working version of the GAN. Makes garbage, but we can do a lot to improve this.

--- 2nd_Project/discriminator.py
+++ 2nd_Project/discriminator.py
@@ -3,88 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Discriminator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # Reproduction purposes (Always generate the same initial weights)
-        torch.manual_seed(0) 
+    def __init__(self, ngpu, ndf, nc):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
 
-        # Make layers 
-        self.conv1 = nn.Conv2d(3, 16, 3) # 3 input channels, 16 output channels, 3x3 convolution
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.conv3 = nn.Conv2d(32, 32, 3)
-
-        self.dropout_02 = nn.Dropout(p=0.2)
-        self.fc1 = nn.Linear(512, 80)
-        self.dropout_05 = nn.Dropout(p=0.5)
-        self.fc2 = nn.Linear(80, 80)
-        self.fc3 = nn.Linear(80, 64)
-        self.fc4 = nn.Linear(64, 10) 	# 10 Output classes
-
-    def forward(self, x, **kwargs):
-        """Second argument to a function call should be the activation function.
-        We can test all standard activation functions here.
-        """
-        if "activation_func" not in kwargs.keys():
-            raise RuntimeError("ERROR: Specify activation function as a keyworded argument.")
-            exit(-1)
-        else:
-            activation_func = kwargs["activation_func"]
-
-        # x = activation_func(self.conv1(x))
-        x = activation_func(self.conv1(x))
-        x = F.max_pool2d(x, (2, 2))
-        x = activation_func(self.conv2(x))
-        x = F.max_pool2d(x, 2) # 2 is the same as (2, 2)
-        x = activation_func(self.conv3(x))
-
-        x = x.view(-1, self.num_flat_features(x)) # Flatten the tensor so we can use the FC-layers
-        if self.dropout:
-            x = self.dropout_02(x)
-        x = activation_func(self.fc1(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = activation_func(self.fc2(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = activation_func(self.fc3(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = F.log_softmax(self.fc4(x), dim=1)
-        return x
-
-    def num_flat_features(self, x):
-        """Calculates the number of features in data
-        e.g. a shape of (80, 5, 5) has num features = 80*5*5."""
-        num = 1
-        for i in x.size()[1:]:
-            num *= i
-        return num
-
-    def evaluate(self, testset, activation_func):
-        """Evaluate the net on a DataLoader testset object"""
-        self.eval() # Tells the net to go into evaluation mode
-        with torch.no_grad():
-            correct, total = 0, 0
-            for data in testset:
-                X, y = data
-                output = self(X, activation_func=activation_func)
-                for idx, out in enumerate(output):
-                    if torch.argmax(out) == y[idx]:
-                        correct += 1
-                    total += 1
-
-        accuracy = correct/total
-        return accuracy
-
-    def get_all_preds(self, testset, activation_func):
-        self.eval()
-        with torch.no_grad():
-            all_preds = torch.tensor([])
-            for data in testset:
-                X, y = data
-                preds = self(X, activation_func=activation_func)
-                all_preds = torch.cat((all_preds, preds), dim=0)
-        return all_preds
-
-    def __repr__(self):
-        return f"GAN({vars(self)['_modules']})"
+    def forward(self, input):
+        return self.main(input)
--- 2nd_Project/generator.py
+++ 2nd_Project/generator.py
@@ -3,88 +3,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # Reproduction purposes (Always generate the same initial weights)
-        torch.manual_seed(0) 
+    def __init__(self, ngpu, ngf, nc, nz):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
 
-        # Make layers 
-        self.conv1 = nn.Conv2d(3, 16, 3) # 3 input channels, 16 output channels, 3x3 convolution
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.conv3 = nn.Conv2d(32, 32, 3)
-
-        self.dropout_02 = nn.Dropout(p=0.2)
-        self.fc1 = nn.Linear(512, 80)
-        self.dropout_05 = nn.Dropout(p=0.5)
-        self.fc2 = nn.Linear(80, 80)
-        self.fc3 = nn.Linear(80, 64)
-        self.fc4 = nn.Linear(64, 10) 	# 10 Output classes
-
-    def forward(self, x, **kwargs):
-        """Second argument to a function call should be the activation function.
-        We can test all standard activation functions here.
-        """
-        if "activation_func" not in kwargs.keys():
-            raise RuntimeError("ERROR: Specify activation function as a keyworded argument.")
-            exit(-1)
-        else:
-            activation_func = kwargs["activation_func"]
-
-        # x = activation_func(self.conv1(x))
-        x = activation_func(self.conv1(x))
-        x = F.max_pool2d(x, (2, 2))
-        x = activation_func(self.conv2(x))
-        x = F.max_pool2d(x, 2) # 2 is the same as (2, 2)
-        x = activation_func(self.conv3(x))
-
-        x = x.view(-1, self.num_flat_features(x)) # Flatten the tensor so we can use the FC-layers
-        if self.dropout:
-            x = self.dropout_02(x)
-        x = activation_func(self.fc1(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = activation_func(self.fc2(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = activation_func(self.fc3(x))
-        if self.dropout:
-            x = self.dropout_05(x)
-        x = F.log_softmax(self.fc4(x), dim=1)
-        return x
-
-    def num_flat_features(self, x):
-        """Calculates the number of features in data
-        e.g. a shape of (80, 5, 5) has num features = 80*5*5."""
-        num = 1
-        for i in x.size()[1:]:
-            num *= i
-        return num
-
-    def evaluate(self, testset, activation_func):
-        """Evaluate the net on a DataLoader testset object"""
-        self.eval() # Tells the net to go into evaluation mode
-        with torch.no_grad():
-            correct, total = 0, 0
-            for data in testset:
-                X, y = data
-                output = self(X, activation_func=activation_func)
-                for idx, out in enumerate(output):
-                    if torch.argmax(out) == y[idx]:
-                        correct += 1
-                    total += 1
-
-        accuracy = correct/total
-        return accuracy
-
-    def get_all_preds(self, testset, activation_func):
-        self.eval()
-        with torch.no_grad():
-            all_preds = torch.tensor([])
-            for data in testset:
-                X, y = data
-                preds = self(X, activation_func=activation_func)
-                all_preds = torch.cat((all_preds, preds), dim=0)
-        return all_preds
-
-    def __repr__(self):
-        return f"GAN({vars(self)['_modules']})"
+    def forward(self, input):
+        return self.main(input)
