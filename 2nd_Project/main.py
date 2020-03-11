7b4b71926441af8ce00956c4e8caa67329b5dc43
ArjanJawahier <arjanjawahier@hotmail.com>
2020-03-11 15:27:20 +0100

    Changed number of epochs

--- 2nd_Project/main.py
+++ 2nd_Project/main.py
@@ -49,7 +49,7 @@ workers = 2
 batch_size = 128
 # Spatial size of training images. All images will be resized to this
 #   size using a transformer.
-image_size = 64
+image_size = 128
 # Number of channels in the training images. For color images this is 3
 nc = 3
 # Size of z latent vector (i.e. size of generator input)
@@ -59,7 +59,7 @@ ngf = 64
 # Size of feature maps in discriminator
 ndf = 64
 # Number of training epochs
-num_epochs = 15
+num_epochs = 100
 # Learning rate for optimizers
 lr = 0.0002
 # Beta1 hyperparam for Adam optimizers
